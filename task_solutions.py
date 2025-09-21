63b9c506b42f59ce5e1360f80ebc5e73450bc7b2
Deniska <deniskaost2005@gmail.com>
2025-09-21 21:41:05 +0300

    Add printer_queue

--- task_solutions.py
+++ task_solutions.py
@@ -1,3 +1,4 @@
+from collections import deque
 def check_brackets(s: str) -> bool:
     stack = []
     pairs = {')': '(', ']': '[', '}': '{'}
@@ -10,4 +11,12 @@ def check_brackets(s: str) -> bool:
             stack.pop()
     return not stack
 
+
+def printer_queue(jobs):
+    queue = deque(jobs)
+    result = []
+    while queue:
+        job = queue.popleft()
+        result.append(job)
+    return result
 
